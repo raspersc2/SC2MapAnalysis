f30a95e116ae811cb3ecfbdd802c81d110885e7d
tom <tomkerrdev@gmail.com>
2025-01-01 10:20:03 +0000

    style: remove unused import

--- map_analyzer/__init__.py
+++ map_analyzer/__init__.py
@@ -1,8 +1,5 @@
 __version__ = "0.2.0"
 
-# flake8: noqa
-from pkg_resources import DistributionNotFound, get_distribution
-
 from .constructs import ChokeArea, MDRamp, VisionBlockerArea
 from .MapData import MapData
 from .Polygon import Polygon
